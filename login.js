228a96e39a97305f6d2529214cc64bb2db1d6cb4
Dokwa7 <foxgrey020@gmail.com>
2025-11-25 10:53:39 +0500

    Added Nav links

--- login.js
+++ login.js
@@ -36,3 +36,18 @@ function validateForm() {
     alert("Validation Successful! Submitting form...");
     return true; 
 }
+document.addEventListener('DOMContentLoaded', () => {
+    // --- Sidebar Toggle Functionality ---
+    const navBtn = document.querySelector(".nav-btn");
+    const closeBtn = document.querySelector(".close-icon");
+    const sidebar = document.querySelector(".sidebar");
+
+    if (navBtn && sidebar && closeBtn) {
+        function toggleSidebar() {
+            sidebar.classList.toggle("show");
+            navBtn.classList.toggle("show"); 
+        }
+        navBtn.addEventListener("click", toggleSidebar);
+        closeBtn.addEventListener("click", toggleSidebar);
+    }
+})
